c6efbec1053915301686ba405a635df422848521
BOUGET Alexandre <abouget68@gmail.com>
2024-06-18 22:17:55 +0200

    build data

--- build-data.py
+++ build-data.py
@@ -24,6 +24,13 @@ def get_author_max(authors):
     return max_nb, name_author
 
 
+def get_nb_x(x, texts):
+    nb = 0
+    for text in texts:
+        nb += text.count(x)
+    return nb
+
+
 def getcsv(file):
     board_name = file.split("/")[-1].split(".")[0]
     csv_file = open(file, 'r')
@@ -34,15 +41,18 @@ def getcsv(file):
     nb_total = len(texts)
     nb_profanity = get_nb_profanity(texts)
     nb_author = get_author_max(authors)
+    nb_link_https = get_nb_x("https://", texts)
+    nb_link_http = get_nb_x("http://", texts)
     nb_word_total = sum([int(i[3]) for i in data if i[0] != "id"])
     return [board_name, nb_total, nb_profanity, nb_author[1], nb_author[0],
-            nb_word_total]
+            nb_word_total, nb_link_https, nb_link_http]
 
 
 csv_file = open("result.csv", 'w')
 writer = csv.writer(csv_file)
 writer.writerow(["board_name", "nb_total", "nb_profanity", "author_active",
-                 "nb_author_active", "nb_word_total"])
+                 "nb_author_active", "nb_word_total", "nb_link_https",
+                 "nb_link_http"])
 for file in os.listdir("data"):
     print(file + " is processing")
     if file.endswith(".csv"):
---
+++ test-ratio.py
@@ -0,0 +1,52 @@
+import csv
+
+csv_reader = csv.reader(open("result.csv", 'r'))
+
+data = list(csv_reader)
+
+
+INDEX_NAME = 0
+INDEX_NB_TOTAL = 1
+INDEX_NB_PROFANITY = 2
+INDEX_AUTHOR_ACTIVE = 3
+INDEX_NB_AUTHOR_ACTIVE = 4
+INDEX_NB_WORD_TOTAL = 5
+INDEX_NB_LINK_HTTPS = 6
+INDEX_NB_LINK_HTTP = 7
+
+
+def get_ratio(list_1, list_2):
+    return [list_1[i] / list_2[i] for i in range(len(list_1))]
+
+
+def get_max(list_1):
+    max_value = 0
+    index = 0
+    for i in range(len(list_1)):
+        if list_1[i] > max_value:
+            max_value = list_1[i]
+            index = i
+    return max_value, index
+
+
+def get_something(data, index):
+    l_get = [int(i[index]) for i in data[1:] if i[0] != "id"]
+    return l_get
+
+
+def get_something_ratio(data, l1_index, l2_index):
+    l1 = get_something(data, l1_index)
+    l2 = get_something(data, l2_index)
+    return get_ratio(l1, l2)
+
+
+def get_name(data, l1_index, l2_index):
+    ratio = get_something_ratio(data, l1_index, l2_index)
+    index = get_max(ratio)[1]
+    return data[index + 1][INDEX_NAME]
+
+
+print("The board with the highest ratio of profanity is: " + get_name(data, INDEX_NB_PROFANITY, INDEX_NB_TOTAL))
+print("The board with the highest ratio of word total is: " + get_name(data, INDEX_NB_WORD_TOTAL, INDEX_NB_TOTAL))
+print("The board with the highest ratio of link https is: " + get_name(data, INDEX_NB_LINK_HTTPS, INDEX_NB_TOTAL))
+print("The board with the highest ratio of link http is: " + get_name(data, INDEX_NB_LINK_HTTP, INDEX_NB_TOTAL))
