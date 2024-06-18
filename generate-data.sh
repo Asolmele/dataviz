51b77de0397c8dbd8e558cf7181229c664e186d4
BOUGET Alexandre <abouget68@gmail.com>
2024-06-18 15:41:08 +0200

    generate data

---
+++ generate-data.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+raw_data=data-raw/wait
+results=data
+
+mkdir $results
+mkdir $raw_data
+
+for d in data-raw/data/*/ ; do
+    [ -L "${d%/}" ] && continue
+    tmp=$raw_data/$(basename $d).txt
+    name=$results/$(basename $d).csv
+    python ./data-raw/decode/merge.py -d $d -o $tmp
+    python ./data-raw/decode/tokenizer.py $tmp $name
+done
+
+
+# python decode/merge.py
