0663fec4f2917abd46c18972383adb041ec795a4
Asolmele <ard.foussac@gmail.com>
2024-06-19 14:42:12 +0200

    feat: update the layout of the app

--- src/layout.py
+++ src/layout.py
@@ -8,7 +8,7 @@ def get_layout():
         "display" : "flex",
         "align-items" : "center",
         "justify-content" : "center",
-        "background-color" : "#FFAFC5"
+        "flex-direction": "column",
     }
 
     layout = html.Div(
@@ -24,21 +24,76 @@ def get_main_container():
     id = "monitor-container"
     style = {
         "display": "flex",
+        "flex-direction": "column",
+        "min-height" : "500px",
+        "background-color" : "#FFCFD2",
+        "width" : "100%"
     }
 
     return html.Div(
         children=[
-            get_profanity_layout(),
-            get_links_layout()
+            get_head_container(),
+            get_collumns_container()
         ],
         id=id,
         style=style)
 
+
+def get_head_container():
+    style= {
+        "min-height" : "500px",
+        "background-color" : "#FFAFC5",
+        "max-width" : "100%",
+        "margin" : "25px"
+    }
+
+    return html.Div(
+        children=get_profanity_layout(),
+        style=style
+    )
+
+def get_collumns_container():
+    style= {
+        "display" : "flex",
+        "min-height" : "500px",
+        "max-width" : "100%",
+    }
+
+    return html.Div(
+        children=[
+            get_links_layout(),
+            get_other_layout(),
+        ],
+        style=style
+    )
+
 def get_profanity_layout():
     style = {
-        "display": "flex",
+        "background-color" : "#FFAFC5",
+
     }
     return html.Div()
 
 def get_links_layout():
-    return html.Div()
+    style = {
+        "margin" : "25px",
+        "background-color" : "#FFAFC5",
+        "min-height": "500px",
+        "max-width" : "50%",
+        "width" : "100%"
+    }
+    return html.Div(
+        style=style
+    )
+
+def get_other_layout():
+    style = {
+        "margin" : "25px",
+        "background-color" : "#FFAFC5",
+        "max-width" : "50%",
+        "min-height": "500px",
+        "width" : "100%"
+    }
+    return html.Div(
+        style=style
+    )
