1b153b5d3bc448b8d6a0718895fa1b4ae73093e1
Asolmele <ard.foussac@gmail.com>
2024-06-19 09:13:22 +0200

    chore: change code architecture

--- src/app.py
+++ src/app.py
@@ -13,4 +13,4 @@ app.layout = [
 ]
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
--- src/views.py
+++ src/views.py
@@ -8,13 +8,7 @@ res_dir = str(pathlib.Path(__file__).parent.parent.resolve().joinpath("results")
 
 df = pd.read_csv(res_dir + "/result.csv")
 
-def update_graph(value):
-    dff = df[df.board_name==value]
-    return px.line(dff, x='nb_total', y='nb_profanity')
-
-    app.run(debug=True)
-
 def get_figure_message_over_profanity():
-  fig = px.scatter(df, x="nb_total", y="nb_profanity", size="nb_word_total", color="board_name",
-    hover_name="board_name", size_max=60)
-  return fig
+    fig = px.scatter(df, x="nb_total", y="nb_profanity", size="nb_word_total", color="board_name",
+        hover_name="board_name", size_max=60)
+    return fig
