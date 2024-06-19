bb194cb2f283df83e1486bf01d8a38d6a8301546
BOUGET Alexandre <abouget68@gmail.com>
2024-06-19 09:08:32 +0200

    get_link

--- get_link.py
+++ get_link.py
@@ -7,7 +7,7 @@ def get_link_from_text(text):
     result = []
     text = re.split(r' |\n|\r|\t', text)
     for i in text:
-        if i.startswith("http"):
+        if i.startswith("http://") or i.startswith("https://"):
             result.append(i)
     return result
 
@@ -15,16 +15,32 @@ def get_link_from_text(text):
 def getlink(file):
     csv_reader = csv.reader(open(file, 'r'))
     data = list(csv_reader)
-    result = []
+    result = {}
+    all_links = []
     for i in data:
         if i[0] == "id":
             continue
-        result.extend(get_link_from_text(i[2]))
-    return result
+        links = get_link_from_text(i[2])
+        all_links.extend(links)
+        for x in links:
+            x = x.split("/")[2]
+            if x in result.keys():
+                result[x] += 1
+            else:
+                result[x] = 1
+
+    def sort_by_value(item):
+        return item[1]
+    result = dict(sorted(result.items(), key=sort_by_value, reverse=True))
+    return [result.items(), all_links]
 
 
 for file in os.listdir("data"):
     print(file + " is processing")
     if file.endswith(".csv"):
         data = getlink("data/" + file)
-        open("link/" + file, 'w').write("\n".join(data))
+        open("results/link/all/" + file, 'w').write("\n".join(data[1]))
+        count_file = csv.writer(open("results/link/count/" + file, 'w'))
+        count_file.writerow(["link", "count"])
+        for i in data[0]:
+            count_file.writerow(i)
